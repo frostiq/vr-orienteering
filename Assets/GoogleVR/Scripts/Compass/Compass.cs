d459398cf5bab079b8cf0b91ec97004b1f9aca58
phoenixIS <savchira9@gmail.com>
2016-12-07 21:07:48 +0300

    Plain compass

---
+++ Assets/GoogleVR/Scripts/Compass/Compass.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Compass : MonoBehaviour {
+
+    public Vector3 NorthDirection;
+    public Transform Player;
+    public Quaternion PlayerDirection;
+    public RectTransform ArrowsLayer;
+
+    void Update()
+    {
+        NorthDirection.z = Player.eulerAngles.y;
+        ChangeArrowNorthDirection();
+    }
+
+    public void ChangeArrowNorthDirection()
+    {
+        Vector3 dir = transform.position - Player.position;
+        PlayerDirection = Quaternion.LookRotation(dir);
+        PlayerDirection.z = -PlayerDirection.y;
+        PlayerDirection.x = 0;
+        PlayerDirection.y = 0;
+        ArrowsLayer.localRotation = PlayerDirection * Quaternion.Euler(NorthDirection);
+    }
+}
