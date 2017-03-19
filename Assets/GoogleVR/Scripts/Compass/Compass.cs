f103337d1e188dea2409f776607a20534ae93473
shogifun <shogifun@yandex.ru>
2017-03-19 19:10:01 +0300

    current azimuth value added

--- Assets/GoogleVR/Scripts/Compass/Compass.cs
+++ Assets/GoogleVR/Scripts/Compass/Compass.cs
@@ -7,10 +7,22 @@ public class Compass : MonoBehaviour {
     public Transform Player;
     public Quaternion PlayerDirection;
     public RectTransform ArrowsLayer;
+    public string Angle;
 
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(580, 30, 800, 200), Angle);
+    }
     void Update()
     {
         NorthDirection.z = Player.eulerAngles.y;
+        //Debug.Log("current angle "+Player.eulerAngles);
+        int tmp =360-(int)Player.eulerAngles.y;
+        if (tmp == 360)
+        {
+            tmp = 0;
+        }
+        Angle = "Текущий азимут " +tmp;
         ChangeArrowNorthDirection();
     }
 
