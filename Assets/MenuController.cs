03b3d3d43fdf8a66c985fdd6ac6663ef30d79e36
shogifun <shogifun@yandex.ru>
2017-03-30 15:49:09 +0300

    about page added

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -5,14 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
-	public void StartTask()
+    public void StartTask()
     {
-        Application.LoadLevel(1);
-        
-        
+        //Application.LoadLevel(1);
+        SceneManager.LoadScene(1);
+
     }
     public void ExitGame()
     {
         Application.Quit();
+    }
+    public void About()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
